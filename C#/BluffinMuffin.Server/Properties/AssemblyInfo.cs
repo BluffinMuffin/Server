a293f17cfe0269ab490213cb32d4e70877a5d76e
Eric Masse <ericmas001@hotmail.com>
2015-07-05 13:07:54 -0400

    Changing Assembly Product & Title

--- C#/BluffinMuffin.Server/Properties/AssemblyInfo.cs
+++ C#/BluffinMuffin.Server/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("0")]
+[assembly: AssemblyTitle("Server BluffinMuffin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BluffinMuffin.Server")]
+[assembly: AssemblyProduct("Server BluffinMuffin")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
