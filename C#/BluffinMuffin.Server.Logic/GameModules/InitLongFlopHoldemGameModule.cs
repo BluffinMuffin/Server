6d7157fdc09f44c16051e9ee9b182ba629613f24
Éric Massé <Ericmas001@gmail.com>
2015-09-14 10:44:35 -0400

    Adding ManilaPoker and SpanishPoker to the list

---
+++ C#/BluffinMuffin.Server.Logic/GameModules/InitLongFlopHoldemGameModule.cs
@@ -0,0 +1,44 @@
+﻿using BluffinMuffin.Server.DataTypes.EventHandling;
+
+namespace BluffinMuffin.Server.Logic.GameModules
+{
+    public class InitLongFlopHoldemGameModule : AbstractInitGameModule
+    {
+        public InitLongFlopHoldemGameModule(PokerGameObserver o, PokerTable table) : base(o, table)
+        {
+        }
+
+        public override void InitModulePlaying()
+        {
+            //Preflop
+            AddModule(new DealMissingCardsToPlayersModule(Observer, Table, Table.Variant.NbCardsInHand));
+            AddModule(new FirstBettingRoundModule(Observer, Table));
+            AddModule(new CumulPotsModule(Observer, Table));
+            
+            //Flop 1
+            AddModule(new DealCardsToBoardModule(Observer, Table, 1));
+            AddModule(new BettingRoundModule(Observer, Table));
+            AddModule(new CumulPotsModule(Observer, Table));
+
+            //Flop 2
+            AddModule(new DealCardsToBoardModule(Observer, Table, 1));
+            AddModule(new BettingRoundModule(Observer, Table));
+            AddModule(new CumulPotsModule(Observer, Table));
+
+            //Flop 3
+            AddModule(new DealCardsToBoardModule(Observer, Table, 1));
+            AddModule(new BettingRoundModule(Observer, Table));
+            AddModule(new CumulPotsModule(Observer, Table));
+
+            //Turn
+            AddModule(new DealCardsToBoardModule(Observer, Table, 1));
+            AddModule(new BettingRoundModule(Observer, Table));
+            AddModule(new CumulPotsModule(Observer, Table));
+            
+            //River
+            AddModule(new DealCardsToBoardModule(Observer, Table, 1));
+            AddModule(new BettingRoundModule(Observer, Table));
+            AddModule(new CumulPotsModule(Observer, Table));
+        }
+    }
+}
---
+++ C#/BluffinMuffin.Server.Logic/GameVariants/ManilaPokerVariant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BluffinMuffin.HandEvaluator;
+using BluffinMuffin.HandEvaluator.HandRankers;
+using BluffinMuffin.HandEvaluator.Selectors;
+using BluffinMuffin.Protocol.DataTypes.Enums;
+using BluffinMuffin.Server.DataTypes;
+using BluffinMuffin.Server.DataTypes.Attributes;
+using BluffinMuffin.Server.Logic.GameModules;
+
+namespace BluffinMuffin.Server.Logic.GameVariants
+{
+    [GameVariant(GameSubTypeEnum.ManilaPoker)]
+    public class ManilaPokerVariant : AbstractGameVariant
+    {
+        public override int NbCardsInHand => 2;
+
+        //TODO: Manila should not have an ace-low straight as a possibility
+        public override EvaluationParams EvaluationParms => new EvaluationParams {HandRanker = new FlushBeatsFullHouseHandRanker(), Selector = new Use2Player3CommunitySelector()};
+
+        public override Type InitModuleType => typeof(InitLongFlopHoldemGameModule);
+        protected override AbstractDealer GenerateDealer()
+        {
+            return new Shuffled32HighCardsDealer();
+        }
+    }
+}
