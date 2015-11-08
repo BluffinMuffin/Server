5c1b317b402c270d57ea26c29da6f22bff438ff3
Éric Massé <Ericmas001@gmail.com>
2015-11-08 14:54:19 -0500

    Tests for PlayerInfo, SeatInfo and TableParams

---
+++ C#/BluffinMuffin.Server.Logic.Test/SeatInfoTests.cs
@@ -0,0 +1,156 @@
+﻿using BluffinMuffin.Protocol.DataTypes;
+using BluffinMuffin.Protocol.DataTypes.Enums;
+using BluffinMuffin.Server.Logic.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BluffinMuffin.Server.Logic.Test
+{
+    [TestClass]
+    public class SeatInfoTests
+    {
+        [TestMethod]
+        public void EmptySeatDoesntHavePlayerPlaying()
+        {
+            //Arrange
+            var s = new SeatInfo();
+
+            //Act
+            var res = s.HasPlayerPlaying();
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void SeatWithAllInPlayerDoesntHavePlayerPlaying()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.AllIn } };
+
+            //Act
+            var res = s.HasPlayerPlaying();
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void SeatWithSitInPlayerDoesntHavePlayerPlaying()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.SitIn } };
+
+            //Act
+            var res = s.HasPlayerPlaying();
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void SeatWithJoinedPlayerDoesntHavePlayerPlaying()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.Joined } };
+
+            //Act
+            var res = s.HasPlayerPlaying();
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void SeatWithZombiePlayerDoesntHavePlayerPlaying()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.Zombie } };
+
+            //Act
+            var res = s.HasPlayerPlaying();
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void SeatWithPlayingPlayerHasPlayerPlaying()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.Playing } };
+
+            //Act
+            var res = s.HasPlayerPlaying();
+
+            //Assert
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void EmptySeatDoesntHavePlayerPlayingOrAllIn()
+        {
+            //Arrange
+            var s = new SeatInfo();
+
+            //Act
+            var res = s.HasPlayerPlayingOrAllIn();
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void SeatWithAllInPlayerDoesntHavePlayerPlayingOrAllIn()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.AllIn } };
+
+            //Act
+            var res = s.HasPlayerPlayingOrAllIn();
+
+            //Assert
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void SeatWithSitInPlayerDoesntHavePlayerPlayingOrAllIn()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.SitIn } };
+
+            //Act
+            var res = s.HasPlayerPlayingOrAllIn();
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void SeatWithJoinedPlayerDoesntHavePlayerPlayingOrAllIn()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.Joined } };
+
+            //Act
+            var res = s.HasPlayerPlayingOrAllIn();
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void SeatWithZombiePlayerDoesntHavePlayerPlayingOrAllIn()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.Zombie } };
+
+            //Act
+            var res = s.HasPlayerPlayingOrAllIn();
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void SeatWithPlayingPlayerHasPlayerPlayingOrAllIn()
+        {
+            //Arrange
+            var s = new SeatInfo { Player = new PlayerInfo { State = PlayerStateEnum.Playing } };
+
+            //Act
+            var res = s.HasPlayerPlayingOrAllIn();
+
+            //Assert
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ C#/BluffinMuffin.Server.Logic.Test/TableParamTests.cs
@@ -0,0 +1,73 @@
+﻿using BluffinMuffin.Protocol.DataTypes;
+using BluffinMuffin.Protocol.DataTypes.Enums;
+using BluffinMuffin.Protocol.DataTypes.Options;
+using BluffinMuffin.Server.Logic.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BluffinMuffin.Server.Logic.Test
+{
+    [TestClass]
+    public class TableParamTests
+    {
+        [TestMethod]
+        public void BuyInUnderMinimumIsInvalid()
+        {
+            //Arrange (Minimum 20*GameSize=200, Maximum 100*GameSize-1999) 
+            var t = new TableParams { GameSize = 10, Lobby = new LobbyOptionsRegisteredMode { IsMaximumBuyInLimited = true } };
+
+            //Act
+            var res = t.IsValidBuyIn(199);
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void BuyInEqualMinimumIsValid()
+        {
+            //Arrange (Minimum 20*GameSize=200, Maximum 100*GameSize-1999) 
+            var t = new TableParams { GameSize = 10, Lobby = new LobbyOptionsRegisteredMode { IsMaximumBuyInLimited = true } };
+
+            //Act
+            var res = t.IsValidBuyIn(200);
+
+            //Assert
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void BuyInBetweenMinimumAndMaximumIsValid()
+        {
+            //Arrange (Minimum 20*GameSize=200, Maximum 100*GameSize-1999) 
+            var t = new TableParams { GameSize = 10, Lobby = new LobbyOptionsRegisteredMode { IsMaximumBuyInLimited = true } };
+
+            //Act
+            var res = t.IsValidBuyIn(500);
+
+            //Assert
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void BuyInEqualMaximumIsValid()
+        {
+            //Arrange (Minimum 20*GameSize=200, Maximum 100*GameSize-1999) 
+            var t = new TableParams { GameSize = 10, Lobby = new LobbyOptionsRegisteredMode { IsMaximumBuyInLimited = true } };
+
+            //Act
+            var res = t.IsValidBuyIn(1000);
+
+            //Assert
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void BuyInOverMaximumIsInvalid()
+        {
+            //Arrange (Minimum 20*GameSize=200, Maximum 100*GameSize-1999) 
+            var t = new TableParams { GameSize = 10, Lobby = new LobbyOptionsRegisteredMode { IsMaximumBuyInLimited = true } };
+
+            //Act
+            var res = t.IsValidBuyIn(1001);
+
+            //Assert
+            Assert.AreEqual(false, res);
+        }
+    }
+}
